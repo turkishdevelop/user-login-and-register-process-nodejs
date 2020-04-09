0b2540f099baafdd59abbe74d2a27eedd8e33f1c
Abdullah <39024317+turkishdevelop@users.noreply.github.com>
2020-04-09 16:35:27 +0300

    veritabanına bağlantı yapıldı.
    veritabanı bağlantısı yapıldı.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
 /*Uygulamanın yönetileceği business katmanı*/
+
+//Web uygulamalarında kullanılan express modülünü app.js dosyasına import ettik.
+const express = require('express');
+require('dotenv').config();
+
+
+/*Uygulama değişkenlerinden aldığımız portu bir değişkene atadık.*/
+const port = process.env.PORT;
+
+/*User işlemlerinin yönlendirileceği router dosyasını dahil ettik*/
+const userRouter = require('./routers/user');
+
+
+
+/*db dosyasını dahil ettik.*/
+require('./db/db');
+
+/*express işlemlerini yönetmek için app değişkenine atadık.*/
+const app = express();
+
+/*gelen istekleri json tipinde route olarak parçaladık. yani her istek burdan geçecek*/
+app.use(express.json());
+
+/*User routera yönlendirme yapılıyor.*/
+app.use("/",function (request,response,next) {
+
+});
+
+/*Uygulamanın 4000 portu üzerinde ayağa kalktığı kısım.*/
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
--- src/db/db.js
+++ src/db/db.js
@@ -1,1 +1,8 @@
 /*Veritabanı işlemlerini yapacağımız ORM katmanı.*/
+const mongoose = require('mongoose');
+//mongo db sunucusuna bağlantı yaptığımız modül
+mongoose.connect(process.env.DB_HOST, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
