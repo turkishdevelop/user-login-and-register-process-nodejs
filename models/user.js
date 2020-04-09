4676b4596fbc2086005323e85e17326004b550f6
Abdullah <39024317+turkishdevelop@users.noreply.github.com>
2020-04-09 15:29:26 +0300

    init
    init node modules

---
+++ models/user.js
@@ -0,0 +1,15 @@
+/*Mongoose modülünü import ediyoruz.*/
+const mongoose = require('mongoose');
+/*Mongoose Schema yı User değişkenine aktarıyoruz.*/
+const User = mongoose.Schema;
+
+/*Mongo DB veritabanına yazmak üzere kullanıcı şemasını tanımlıyoruz.*/
+const user = new User({
+    name:{type:String},
+    username:{type:String},
+    email:{type:String},
+    password:{type:String}
+
+});
+/*Hazırladığımız kullanıcı modülünü kullanmak için export ediyoruz*/
+module.exports=mongoose.model("User",user);
